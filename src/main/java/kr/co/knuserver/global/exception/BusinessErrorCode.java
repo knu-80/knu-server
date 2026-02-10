436354a554393b73ca2d5b1a35e828e7cf9407e2
wlgns12370 <knu12370@knu.ac.kr>
2026-02-10 12:42:37 +0900

    fix: 불명확한 공통 에러 제거

--- src/main/java/kr/co/knuserver/global/exception/BusinessErrorCode.java
+++ src/main/java/kr/co/knuserver/global/exception/BusinessErrorCode.java
@@ -36,26 +36,21 @@ public enum BusinessErrorCode implements ErrorCode {
      */
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "C005", "접근 권한이 없습니다."),
 
-    /*
-     * 404 NOT_FOUND: 리소스를 찾을 수 없음
-     */
-    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "C006", "대상을 찾을 수 없습니다."),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "C007", "요청한 리소스를 찾을 수 없습니다."),
 
     /*
      * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
      */
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C008", "허용되지 않은 메서드입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C007", "허용되지 않은 메서드입니다."),
 
     /*
      * 409 CONFLICT: 리소스 충돌 (중복 데이터 등)
      */
-    ALREADY_EXISTS(HttpStatus.CONFLICT, "C009", "이미 존재하는 데이터입니다."),
+    ALREADY_EXISTS(HttpStatus.CONFLICT, "C008", "이미 존재하는 데이터입니다."),
 
     /*
      * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C010", "서버 내부 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C009", "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
