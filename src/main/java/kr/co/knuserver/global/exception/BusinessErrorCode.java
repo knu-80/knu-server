2cead21d644c352e020bab6af9720a85f066840e
GyunHee <ef3232@naver.com>
2026-02-17 17:43:07 +0900

    feat: 주막 NOT_FOUND 예외 추가

--- src/main/java/kr/co/knuserver/global/exception/BusinessErrorCode.java
+++ src/main/java/kr/co/knuserver/global/exception/BusinessErrorCode.java
@@ -38,6 +38,11 @@ public enum BusinessErrorCode implements ErrorCode {
 
 
     /*
+     * 404 NOT_FOUND: 리소스를 찾을 수 없음
+     */
+    PUB_BOOTH_NOT_FOUND(HttpStatus.NOT_FOUND, "C301", "해당 주점을 찾을 수 없습니다."),
+
+    /*
      * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
      */
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C007", "허용되지 않은 메서드입니다."),
