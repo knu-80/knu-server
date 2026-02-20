31996d0295184c892ce3f5b0bfcce508a355a6cb
Hyun <hyun_7586@naver.com>
2026-02-20 19:30:09 +0900

    refactor: BoothApiController에서도 ApiResponse 공통 응답 객체를 사용하도록 수정

--- src/main/java/kr/co/knuserver/presentation/booth/BoothApiController.java
+++ src/main/java/kr/co/knuserver/presentation/booth/BoothApiController.java
@@ -2,6 +2,7 @@ package kr.co.knuserver.presentation.booth;
 
 
 import kr.co.knuserver.application.booth.BoothService;
+import kr.co.knuserver.global.exception.ApiResponse;
 import kr.co.knuserver.presentation.booth.dto.BoothInfoResponseDto;
 import kr.co.knuserver.presentation.booth.dto.BoothRegisterRequestDto;
 import kr.co.knuserver.presentation.booth.dto.BoothUpdateRequestDto;
@@ -24,7 +25,6 @@ public class BoothApiController {
 
     private final BoothService boothService;
 
-
     // 가두모집 부스 생성
     @PostMapping
     public ResponseEntity<ApiResponse<BoothInfoResponseDto>> createBooth(
@@ -32,36 +32,37 @@ public class BoothApiController {
     ) {
         BoothInfoResponseDto result = boothService.registerBooth(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(result);
+            .body(ApiResponse.success(result));
     }
 
     // 가두모집 부스 단건 조회
     @GetMapping("/{booth-id}")
-    public ResponseEntity<BoothInfoResponseDto> getBooth(
+    public ResponseEntity<ApiResponse<BoothInfoResponseDto>> getBooth(
         @PathVariable(name = "booth-id") Long boothId
     ) {
         BoothInfoResponseDto result = boothService.getBooth(boothId);
         return ResponseEntity.status(HttpStatus.OK)
-            .body(result);
+            .body(ApiResponse.success(result));
     }
 
     // 가두모집 부스 수정
     @PatchMapping("/update/{booth-id}")
-    public ResponseEntity<BoothInfoResponseDto> updateBooth(
+    public ResponseEntity<ApiResponse<BoothInfoResponseDto>> updateBooth(
         @PathVariable(name = "booth-id") Long boothId,
         @RequestBody BoothUpdateRequestDto request
     ) {
         BoothInfoResponseDto result = boothService.updateBooth(boothId, request);
         return ResponseEntity.status(HttpStatus.OK)
-            .body(result);
+            .body(ApiResponse.success(result));
     }
 
     // 가두모집 부스 삭제
     @DeleteMapping("/{booth-id}")
-    public ResponseEntity<?> deleteBooth(
+    public ResponseEntity<ApiResponse<?>> deleteBooth(
         @PathVariable(name = "booth-id") Long boothId
     ) {
         boothService.deleteBooth(boothId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success());
     }
 }
