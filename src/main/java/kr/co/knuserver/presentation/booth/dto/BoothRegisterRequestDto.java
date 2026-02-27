43395406d0baed4f20969acdeebf55f38543df56
Hyun <hyun_7586@naver.com>
2026-02-27 16:58:55 +0900

    refactor: Booth request dto에 validation 제약 조건 추가
    - registerDto에 @NotNull 등의 제약 조건 추가
    - updateDto에 @Size 제약 조건 추가

--- src/main/java/kr/co/knuserver/presentation/booth/dto/BoothRegisterRequestDto.java
+++ src/main/java/kr/co/knuserver/presentation/booth/dto/BoothRegisterRequestDto.java
@@ -1,13 +1,25 @@
 package kr.co.knuserver.presentation.booth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.co.knuserver.domain.booth.entity.Booth;
 import kr.co.knuserver.domain.booth.entity.BoothDivision;
 
 public record BoothRegisterRequestDto(
     Long id,
+
+    @NotNull(message = "부스 운영자 member ID는 필수입니다.")
     Long memberId,
+
+    @NotNull(message = "부스 번호는 필수입니다.")
     Integer boothNumber,
+
+    @NotBlank(message = "부스 이름은 필수입니다.")
+    @Size(max = 50, message = "부스 이름은 최대 50자까지 입력 가능합니다.")
     String name,
+
+    @NotNull(message = "부스 분과는 필수입니다.")
     BoothDivision division,
     String description,
     String applyLink,
--- src/main/java/kr/co/knuserver/presentation/booth/dto/BoothUpdateRequestDto.java
+++ src/main/java/kr/co/knuserver/presentation/booth/dto/BoothUpdateRequestDto.java
@@ -1,10 +1,13 @@
 package kr.co.knuserver.presentation.booth.dto;
 
+import jakarta.validation.constraints.Size;
 import kr.co.knuserver.domain.booth.entity.BoothDivision;
 
 public record BoothUpdateRequestDto(
     Long memberId,
     Integer boothNumber,
+
+    @Size(max = 50, message = "부스 이름은 최대 50자까지 입력 가능합니다.")
     String name,
     BoothDivision division,
     String description,
