e5ab8d7d878c6bfcb1a5eec8899cbfe4f7e7f145
milk-stone <ywshero@naver.com>
2026-02-28 16:15:25 +0900

    fix: admin API 경로에 버전 정보 추가

--- src/main/java/kr/co/knuserver/presentation/auth/controller/AdminAuthController.java
+++ src/main/java/kr/co/knuserver/presentation/auth/controller/AdminAuthController.java
@@ -17,7 +17,7 @@ import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin")
+@RequestMapping("/api/v1/admin")
 public class AdminAuthController {
 
     private final AdminAuthService adminAuthService;
