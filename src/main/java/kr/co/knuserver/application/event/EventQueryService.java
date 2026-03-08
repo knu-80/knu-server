a095df83f34497f2d02079aefb9464fddc6ba707
Hyun <hyun_7586@naver.com>
2026-03-08 20:26:57 +0900

    refactor: Event 이미지 조회 로직을 Booth 도메인과 동일하게 수정
    - S3 uploader 사용하도록 수정

--- src/main/java/kr/co/knuserver/application/event/EventQueryService.java
+++ src/main/java/kr/co/knuserver/application/event/EventQueryService.java
@@ -1,6 +1,7 @@
 package kr.co.knuserver.application.event;
 
 import java.util.List;
+import kr.co.knuserver.domain.event.entity.Event;
 import kr.co.knuserver.domain.event.entity.EventType;
 import kr.co.knuserver.domain.event.repository.EventRepository;
 import kr.co.knuserver.presentation.event.dto.EventResponseDto;
@@ -13,14 +14,22 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 public class EventQueryService {
+
+    private final EventReader eventReader;
     private final EventRepository eventRepository;
 
+    // 이벤트 단건 조회
+    public EventResponseDto getEvent(Long eventId) {
+        Event event = eventReader.getEventOrThrow(eventId);
+        return EventResponseDto.fromEntity(event);
+    }
+
     // 이벤트 리스트 조회(type에 따라)
-    public List<EventResponseDto> getEventListByEventType(EventType eventType){
-        return eventRepository.findAllByEventTypeAndIsActiveTrue(eventType)
-            .stream()
+    public List<EventResponseDto> getEventListByEventType(EventType eventType) {
+        List<Event> events = eventRepository.findAllByEventTypeAndIsActiveTrue(eventType);
+
+        return events.stream()
             .map(EventResponseDto::fromEntity)
             .toList();
     }
-
 }
