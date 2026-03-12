9f49fe064b245a445b455cbdb6e2afe2fba9e044
wlgns12370 <knu12370@knu.ac.kr>
2026-03-12 14:59:18 +0900

    refactor: BoothReader 쿼리 transaction 참여하도록 변경

--- src/main/java/kr/co/knuserver/application/booth/BoothQueryService.java
+++ src/main/java/kr/co/knuserver/application/booth/BoothQueryService.java
@@ -8,6 +8,8 @@ import kr.co.knuserver.domain.booth.entity.Booth;
 import kr.co.knuserver.domain.booth.entity.BoothImage;
 import kr.co.knuserver.domain.booth.repository.BoothImageRepository;
 import kr.co.knuserver.domain.booth.repository.BoothRepository;
+import kr.co.knuserver.global.exception.BusinessErrorCode;
+import kr.co.knuserver.global.exception.BusinessException;
 import kr.co.knuserver.presentation.booth.dto.BoothInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,28 +21,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 public class BoothQueryService {
 
-    private final BoothReader boothReader;
     private final BoothRepository boothRepository;
     private final BoothImageRepository boothImageRepository;
 
-    // 부스 조회
     public BoothInfoResponseDto getBooth(Long boothId) {
-        Booth booth = boothReader.getBoothOrThrow(boothId);
+        Booth booth = boothRepository.findById(boothId)
+                .orElseThrow(() -> new BusinessException(BusinessErrorCode.BOOTH_NOT_FOUND));
         List<String> imageUrls = boothImageRepository.findAllByBoothId(boothId).stream()
             .map(BoothImage::getImageUrl)
             .toList();
         return BoothInfoResponseDto.fromEntity(booth, imageUrls);
     }
 
-    // 주어진 키워드를 바탕으로 부스 리스트 조회
     public List<BoothInfoResponseDto> searchBoothsByKeyword(String keyword) {
         List<Booth> booths;
 
-        // 키워드가 없거나 공백이면 전체 목록 반환
         if (keyword == null || keyword.isBlank()) {
             booths = boothRepository.findByIsActiveTrue();
         }
-        // 키워드가 있으면 해당 키워드로 검색
         else {
             booths = boothRepository.searchByKeyword(keyword);
         }
@@ -56,5 +54,4 @@ public class BoothQueryService {
             .map(booth -> BoothInfoResponseDto.fromEntity(booth, imageUrlsMap.getOrDefault(booth.getId(), Collections.emptyList())))
             .toList();
     }
-
 }
