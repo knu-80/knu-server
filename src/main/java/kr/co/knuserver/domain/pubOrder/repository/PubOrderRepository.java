1512eb9b054df7970fd6b964c0568c03c49b7fb1
jhssong <jhssong02@gmail.com>
2026-03-13 16:46:43 +0900

    chore: PubOrderRepository에 불필요한 import문 제거

--- src/main/java/kr/co/knuserver/domain/pubOrder/repository/PubOrderRepository.java
+++ src/main/java/kr/co/knuserver/domain/pubOrder/repository/PubOrderRepository.java
@@ -2,7 +2,6 @@ package kr.co.knuserver.domain.pubOrder.repository;
 
 import java.util.List;
 import kr.co.knuserver.domain.pubOrder.entity.PubOrder;
-import kr.co.knuserver.domain.pubTableSession.entity.PubTableSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PubOrderRepository extends JpaRepository<PubOrder, Long> {
